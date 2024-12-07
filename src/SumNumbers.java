178da8b57a0c815f8011fcc2ab36eea8bd29d82d
lviniciuszup <luiz.vincius@zup.com.br>
2024-12-07 16:37:53 -0300

    Add program to calculate the sum from 1 to n

--- src/SumNumbers.java
+++ src/SumNumbers.java
@@ -1,5 +1,15 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class SumNumbers {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner numberscr = new Scanner(System.in);
+        System.out.print("Me informe um numero para calcular a soma: ");
+        int n = numberscr.nextInt();
+        int soma = 0;
+        for (int i = 1; i <= n; i++) {
+            soma += i;
+            System.out.println("A soma de 1 até " + n + " é: " + soma);
+        }
     }
 }
