43ba6b1f70e664ef53e7f60219345189f9160221
lviniciuszup <luiz.vincius@zup.com.br>
2024-12-08 20:16:33 -0300

    Prime number check for integer

--- src/PrimeNumbers.java
+++ src/PrimeNumbers.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class PrimeNumbers {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número inteiro: ");
+        int numero = scanner.nextInt();
+
+        boolean isPrime = true;
+
+        if (numero <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= Math.sqrt(numero); i++) {
+                if (numero % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+        System.out.println("O número " + numero + " é primo? " + isPrime);
+
+        scanner.close();
     }
 }
