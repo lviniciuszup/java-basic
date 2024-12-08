39f6edd1d4b7dfbd3418a38ec95d89cfc719522c
lviniciuszup <luiz.vincius@zup.com.br>
2024-12-08 20:25:52 -0300

    Add multiples calculation

--- src/MultiplesFinder.java
+++ src/MultiplesFinder.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class MultiplesFinder {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o número n: ");
+        int n = scanner.nextInt();
+        System.out.print("Digite o limite m: ");
+        int m = scanner.nextInt();
+
+        for (int i = n; i <= m; i += n) {
+            System.out.print(i + " ");
+        }
+
+        scanner.close();
     }
 }
