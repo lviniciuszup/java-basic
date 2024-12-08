1ed578899dec2f6600b897a206108a007992e792
lviniciuszup <luiz.vincius@zup.com.br>
2024-12-08 20:28:16 -0300

    Binary to decimal

--- src/BinaryToDecimal.java
+++ src/BinaryToDecimal.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class BinaryToDecimal {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número binário: ");
+        String binario = scanner.next();
+
+        int decimal = Integer.parseInt(binario, 2);
+
+        System.out.println("Número decimal: " + decimal);
+
+        scanner.close();
     }
 }
