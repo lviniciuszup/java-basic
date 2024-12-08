1146c3d11e8fa8aabb3bfca675f4c26b3534c7ee
lviniciuszup <luiz.vincius@zup.com.br>
2024-12-08 20:20:54 -0300

    Power calculation using for loop

--- src/PowerCalculation.java
+++ src/PowerCalculation.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class PowerCalculation {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite a base: ");
+        int base = scanner.nextInt();
+        System.out.print("Digite o expoente: ");
+        int expoente = scanner.nextInt();
+
+        int resultado = 1;
+
+        for (int i = 1; i <= expoente; i++) {
+            resultado *= base;
+        }
+
+        System.out.println("Resultado: " + resultado);
+
+        scanner.close();
     }
 }
